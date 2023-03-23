46d9a8323c68c4d344636aa2769df36a85f3a6b5
DESKTOP-5TCD4T0\EQUIPO <f>
2023-03-23 02:12:20 +0100

    200323 1.2

--- LM_Matrícula_alumno_/src/main/webapp/script/script.js
+++ LM_Matrícula_alumno_/src/main/webapp/script/script.js
@@ -1,1 +1,25 @@
-//cambio comentado
+//IMPORTANTE PUNTO Y COMA DETRAS DEL OBJETOS Y SUS ATRIBUTOS
+
+
+	
+	function Matricula (nombre,apellido,telefono,marca,modelo){
+		
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.telefono=telefono;
+		this.marca=marca;
+		this.modelo=modelo;
+			
+	}	
+	
+	
+	
+function matri(){	
+	
+	var m = new Matricula("diego","prada","124565351","asus","241-1as");
+	
+	console.log(m.nombre);
+	console.log(m.apellido);
+	
+}	
+
